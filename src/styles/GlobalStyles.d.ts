fcb9916774fec2b418a35997faeedc51d61bb0fc
eunbean <ds5bean23@gmail.com>
2022-06-19 19:00:46 +0900

    feat: add google  font

--- src/styles/GlobalStyles.d.ts
+++ src/styles/GlobalStyles.d.ts
@@ -2,6 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
   html, body, div, p {
     margin: 0;
     padding: 0;
