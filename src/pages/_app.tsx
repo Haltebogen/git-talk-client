91b3cdd8f21cf4703a9d10effbaa7748789b41ea
Kimkyungmin123 <rud3302@naver.com>
2022-06-22 14:12:40 +0900

    chore: viewport tag 위치 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
+import Head from 'next/head';
 import GlobalStyles from '../styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
-import { theme } from '../styles/theme';
-
+import theme from '../styles/theme';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <body>
             <Main />
             <NextScript />
