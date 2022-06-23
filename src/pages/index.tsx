bd6150a2e75235a89f4dad3ede6c2d6793a5393a
eunbean <ds5bean23@gmail.com>
2022-06-23 21:38:45 +0900

    feat: PlusBtn, Bg 컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next';
 import React from 'react';
-
+import PlusBtn from '../components/atoms/buttons/PlusBtn';
+import BackColor from '../components/atoms/resources/Background';
 const Home: NextPage = () => {
-  return <div>HOME</div>;
+  return (
+    // <div>
+    <BackColor>
+      <PlusBtn />
+      <div>HO~~ME</div>
+    </BackColor>
+    // </div>
+  );
 };
 
 export default Home;
