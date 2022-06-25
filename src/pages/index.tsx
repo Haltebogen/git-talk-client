1d1b69cfa81b992e6a6eb1eff10b30b8acb2e88b
eunbean <ds5bean23@gmail.com>
2022-06-26 00:20:13 +0900

    feat: 메인 로고 컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import BasicButton from '@/buttons/BasicButton';
+import HomeLogo from '@/logos/HomeLogo';
 import type { NextPage } from 'next';
 import { Container } from 'styles/inedx';
-
 const Home: NextPage = () => {
   return (
     <Container>
-      <h2>GIT TALK</h2>
-      <p>GITHUB 유저들과 채팅할 수 있는 공간</p>
-      <BasicButton ariaLabel="login" type="submit" onClick={() => console.log('로그인!')}>
-        GITHUB 로그인
-      </BasicButton>
+      <HomeLogo />
+      <div className="textContainer">
+        <h2>GIT TALK</h2>
+        <p>GITHUB 유저들과 채팅할 수 있는 공간</p>
+        <BasicButton ariaLabel="login" type="submit" onClick={() => console.log('로그인!')}>
+          GITHUB 로그인
+        </BasicButton>
+      </div>
     </Container>
   );
 };
