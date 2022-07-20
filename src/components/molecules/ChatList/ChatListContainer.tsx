ebb73144e50f0405e8f955df9b21eeb07a363ee4
Kimkyungmin123 <rud3302@naver.com>
2022-07-20 10:55:06 +0900

    feat: ChatList organisms component 생성

--- src/components/molecules/ChatList/ChatListContainer.tsx
+++ src/components/molecules/ChatList/ChatListContainer.tsx
@@ -2,6 +2,7 @@ import ChatListBox from '@/boxes/ChatListBox';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 0.4375rem;
---
+++ src/components/organisms/ChatList/ChatList.tsx
@@ -0,0 +1,13 @@
+import SearchBox from '@/boxes/SearchBox';
+import ChatListContainer from '@/molecules/ChatList/ChatListContainer';
+
+const ChatList = () => {
+  return (
+    <div>
+      <SearchBox />
+      <ChatListContainer />
+    </div>
+  );
+};
+
+export default ChatList;
