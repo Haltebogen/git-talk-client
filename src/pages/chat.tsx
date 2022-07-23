94d71ca1e818c685e0fcf3148f47ec710a3f36d3
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-07-23 22:36:05 +0900

    feat : chat page 생성

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,12 +1,16 @@
 import SearchBox from '@/boxes/SearchBox';
 import ChatListContainer from '@/molecules/ChatList/ChatListContainer';
+import ChatRoomContainer from '@/organisms/ChatRoom/ChatRoomContainer';
+import NavBarContainer from '@/organisms/NavBar/NavBarContainer';
 import { NextPage } from 'next';
 
 const Chat: NextPage = () => {
   return (
     <div>
+      <NavBarContainer />
       <SearchBox />
       <ChatListContainer />
+      <ChatRoomContainer />
     </div>
   );
 };
