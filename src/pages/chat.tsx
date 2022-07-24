891529d9a12fffb966adc995248c89d2f39c9fd2
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-07-24 21:01:59 +0900

    chore : Chat Page 수정

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,7 +1,4 @@
-import SearchBox from '@/boxes/SearchBox';
-import ChatListContainer from '@/molecules/ChatList/ChatListContainer';
 import ChatRoomContainer from '@/organisms/ChatRoom/ChatRoomContainer';
-import NavBarContainer from '@/organisms/NavBar/NavBarContainer';
 import ChatList from '@/organisms/ChatList/ChatList';
 import NavBarLayout from '@/organisms/NavBar/NavBarLayout';
 import { NextPage } from 'next';
@@ -9,9 +6,6 @@ import { NextPage } from 'next';
 const Chat: NextPage = () => {
   return (
     <div>
-      <NavBarContainer />
-      <SearchBox />
-      <ChatListContainer />
       <ChatRoomContainer />
       <NavBarLayout>
         <ChatList />
