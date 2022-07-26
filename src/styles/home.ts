42bf87af2eef056a8a04e4386cacf99b2f71ba95
Kimkyungmin123 <rud3302@naver.com>
2022-07-26 21:37:44 +0900

    chore: style파일 분리

---
+++ src/styles/home.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const Area = styled.div`
+  display: flex;
+  flex-grow: 1;
+  width: 100%;
+  justify-content: center;
+  margin-top: 5.625rem;
+`;
