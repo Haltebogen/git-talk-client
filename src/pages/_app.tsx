75ffa00be2eb01394052cb4c4d258debbeba768e
Kimkyungmin123 <rud3302@naver.com>
2022-08-08 14:14:26 +0900

    chore: title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import GlobalStyles from '../styles/GlobalStyle';
+import GlobalStyle from '../styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import type { AppProps } from 'next/app';
@@ -7,8 +7,9 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyle />
       <Head>
+        <title>Git-Talk</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
