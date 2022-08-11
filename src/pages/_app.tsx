01fd396d034f4d6788ddea59975998dc35dcca4a
Kimkyungmin123 <rud3302@naver.com>
2022-08-11 23:43:47 +0900

    chore: wrapper 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,19 @@ import GlobalStyle from '../styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import type { AppProps } from 'next/app';
+import wrapper from '../store/configureStore';
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Head>
         <title>Git-Talk</title>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
+export default wrapper.withRedux(App);
