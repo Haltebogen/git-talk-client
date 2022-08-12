9c45e2ace86e431fae2c22b8a2f9c77b44bf7d91
Kimkyungmin123 <rud3302@naver.com>
2022-08-13 02:25:38 +0900

    refactor: atoms/boxs refactoring

--- src/components/molecules/FollowerList/FollowerListContainer.tsx
+++ src/components/molecules/FollowerList/FollowerListContainer.tsx
@@ -1,23 +1,79 @@
-import FollowerListBox from '@/boxes/FollowerListBox';
-import styled from 'styled-components';
+import { BoxLayout, BoxProps } from '@/boxes/Box';
+import styled, { css } from 'styled-components';
+import Profile from '@/icons/profile_img.svg';
 
-export const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 0.4375rem;
+const FollowerListBox = styled(BoxLayout)`
+  gap: 1.3125rem;
   align-items: center;
+  border: none;
+  max-width: 36.25rem;
+  max-height: 5.875rem;
+
+  .profileImg {
+    padding: 1.25rem 0 1.25rem 2.1875rem;
+    transform: scale(0.2);
+    width: 100%;
+    max-width: 3.25rem;
+
+    svg {
+      padding-left: 2.8125rem;
+    }
+  }
 `;
 
-const FollowerListContainer = () => {
-  // 확인을 위해 임시로 컴포넌트 반복 삽입
+export const Name = styled.div`
+  ${({ theme }) => {
+    const { fontSize } = theme;
+    return css`
+      font-size: ${fontSize.sm};
+      display: flex;
+      width: 100%;
+      font-weight: 700;
+      justify-content: flex-start;
+      padding-left: 2.25rem;
+    `;
+  }}
+`;
+
+export const StatusMessage = styled.div`
+  ${({ theme }) => {
+    const { fontSize } = theme;
+    return css`
+      font-size: ${fontSize.sm}
+      padding-left: 2.25rem;;
+      display: flex;
+      justify-content: flex-start;
+      width: 100%;
+      height:100%
+      max-width: 12.5rem;
+      min-width: 8.75rem;
+    `;
+  }}
+`;
+
+interface FollowerListContainer extends BoxProps {
+  name: string;
+  profileImg: string | null;
+  statusMessage?: string;
+}
+
+const STATUSMSG_MAX_LENGTH = 20;
+
+const FollowerListContainer = ({ name, profileImg, statusMessage, onClick }: FollowerListContainer) => {
   return (
-    <Container>
-      <FollowerListBox name="kimkyungmin" profileImg={null} statusMessage="상태메시지" />{' '}
-      <FollowerListBox name="kimkyungmin" profileImg={null} statusMessage="상태메시지" />{' '}
-      <FollowerListBox name="kimkyungmin" profileImg={null} statusMessage="상태메시지" />{' '}
-      <FollowerListBox name="kimkyungmin" profileImg={null} statusMessage="상태메시지" />{' '}
-    </Container>
+    <FollowerListBox boxType="list" onClick={onClick}>
+      {profileImg ? (
+        <div className="profileImg">{profileImg}</div>
+      ) : (
+        <div className="profileImg">
+          <Profile />
+        </div>
+      )}
+      <Name>{name}</Name>
+      <StatusMessage>
+        {statusMessage && (statusMessage.length > STATUSMSG_MAX_LENGTH ? `${statusMessage.slice(0, STATUSMSG_MAX_LENGTH)} ...` : statusMessage)}
+      </StatusMessage>
+    </FollowerListBox>
   );
 };
 
--- src/components/organisms/FollowerProfile/FollowerProfile.tsx
+++ src/components/organisms/FollowerProfile/FollowerProfile.tsx
@@ -1,22 +1,38 @@
-import FollowerBackgroundBox from '@/boxes/FollowerBackgroundBox';
+import { BoxLayout } from '@/boxes/Box';
 import FollowerInfo from '@/contents/FollowerInfo';
 import ProfileMenu from '@/molecules/followerProfile/ProfileMenu';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const FollowerBackgroundBox = styled(BoxLayout)`
+  ${({ theme }) => {
+    const { colors } = theme;
+    return css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      background-color: ${colors.profilebgcolor};
+      max-width: 44.3125rem;
+      max-height: 51.25rem;
+      gap: 5.2rem;
+    `;
+  }}
+`;
 
 const Info = styled.div`
-  padding: 14.5rem 0 1.25rem 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
 
 const FollowerProfile = () => {
   return (
-    <>
-      <FollowerBackgroundBox>
-        <Info>
-          <FollowerInfo name="김oo" profileImg={null} stateMessages="상태 메시지 입니다." />
-        </Info>
-        <ProfileMenu />
-      </FollowerBackgroundBox>
-    </>
+    <FollowerBackgroundBox boxType="backrgound">
+      <Info>
+        <FollowerInfo name="김oo" profileImg={null} stateMessages="상태 메시지 입니다." />
+      </Info>
+      <ProfileMenu />
+    </FollowerBackgroundBox>
   );
 };
 
