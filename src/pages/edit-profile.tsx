d5869f6916faf40a12f2c787a86978cc4b8016e8
Kimkyungmin123 <rud3302@naver.com>
2022-08-12 18:48:28 +0900

    chore: 상태 관리 추가

--- src/pages/edit-profile.tsx
+++ src/pages/edit-profile.tsx
@@ -1,12 +1,21 @@
 import EditMyProfile from '@/organisms/editMyProfile/EditMyProfile';
 import NavBarLayout from '@/organisms/navBar/NavBarLayout';
+import useInput from 'hooks/useInput';
 import { NextPage } from 'next';
 
 const Editprofile: NextPage = () => {
+  const [value, onChangeValue, setValue] = useInput('');
   return (
     <div>
       <NavBarLayout>
-        <EditMyProfile />
+        <EditMyProfile
+          value={value}
+          onChange={(event: any) => {
+            const { value } = event.target;
+            onChangeValue;
+            setValue(value);
+          }}
+        />
       </NavBarLayout>
     </div>
   );
