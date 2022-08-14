f302a1a15d172afcfac865ce1acd3cf447de969a
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-08-14 21:19:15 +0900

    feat: 채팅상대 프로필 이미지 추가

--- src/components/molecules/ChatRoom/ChatBox.tsx
+++ src/components/molecules/ChatRoom/ChatBox.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import MsgBox from '@/boxes/MsgBox';
+import ProfileImg from '@/icons/profile_img.svg';
 
 export const Container = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
   .receiver {
     display: flex;
-  }
-  .profile {
-    display: flex;
-    flex-direction: column;
     justify-content: flex-start;
-    height: 30%;
+    gap: 1.25rem;
+    .profileImg {
+      transform: scale(0.1);
+      width: 100%;
+      max-width: 1.25rem;
+      max-height: 1rem;
+    }
+  }
+  .sender {
+    display: flex;
+    justify-content: flex-end;
   }
 `;
 
@@ -22,21 +26,31 @@ export const Space = styled.div`
 `;
 
 export interface ChatBoxProps {
+  profileImg: string | null;
   isMe: boolean;
   message: string;
 }
 
-const ChatBox = ({ isMe, message }: ChatBoxProps) => {
+const ChatBox = ({ profileImg, isMe, message }: ChatBoxProps) => {
   return (
     <>
       {isMe ? (
         <Container>
           <Space />
-          <MsgBox message={message} isMe={isMe} />
+          <div className="sender">
+            <MsgBox message={message} isMe={isMe} />
+          </div>
         </Container>
       ) : (
         <Container>
           <div className="receiver">
+            {profileImg ? (
+              <div className="profileImg">{profileImg}</div>
+            ) : (
+              <div className="profileImg">
+                <ProfileImg />
+              </div>
+            )}
             <MsgBox message={message} isMe={isMe} />
           </div>
           <Space />
