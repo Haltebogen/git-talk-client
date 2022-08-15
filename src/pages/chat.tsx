95b905960b4555041a9a7c4b54b721af31397bad
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-08-15 15:55:58 +0900

    refactor: chat pages conflict 해결

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -6,20 +6,6 @@ import ChatList from '@/organisms/ChatList/ChatList';
 import NavBarLayout from '@/organisms/NavBar/NavBarLayout';
 import { NextPage } from 'next';
 import { Container, Area } from 'styles/chat';
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-export const Area = styled.div`
-  display: flex;
-  flex-grow: 1;
-  width: 100%;
-  justify-content: center;
-  margin-top: 5.625rem;
-`;
 
 const Chat: NextPage = () => {
   const { isShown, openModal, closeModal } = useModal();
