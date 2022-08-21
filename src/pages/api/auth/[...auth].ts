977ba48124a0117827f2c7e6d9605e5283ddbcf3
Kimkyungmin123 <rud3302@naver.com>
2022-08-21 13:11:32 +0900

    chore: github social login

--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -4,30 +4,49 @@ import axios from 'axios';
 const baseUrl = 'https://github.com/login/oauth/access_token';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { code } = req.query;
   try {
-    const { data: requestToken } = await axios.post(
-      baseUrl,
-      {
-        client_id: process.env.NEXT_PUBLIC_GITHUB_ID,
-        client_secret: process.env.GITHUB_SECRET,
-        code,
-      },
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-    );
+    const { data, userinfo } = await getUser(req);
+    console.log(data);
+    console.log(userinfo);
 
-    console.log(`requestToken`, requestToken);
-
-    // api 구현되면 넘겨주기
-    // const { access_token, token_type, scope } = requestToken;
-
-    return res.status(201).redirect('/home');
+    return res.status(200).redirect('/home');
   } catch (err) {
     console.error(err);
-    return res.redirect(500, '/?loginError=서버 에러로 인한 로그인 실패');
+    return res.status(500).redirect('/non-login');
   }
+}
+
+async function getUser(req: NextApiRequest) {
+  const { code } = req.query;
+  const { data: requestToken } = await axios.post(
+    baseUrl,
+    {
+      client_id: process.env.NEXT_PUBLIC_GITHUB_ID,
+      client_secret: process.env.GITHUB_SECRET,
+      code,
+    },
+    {
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+  );
+
+  console.log(`requestToken`, requestToken);
+
+  const { access_token, token_type, scope } = requestToken;
+
+  const { data } = await axios.post(process.env.NEXT_PUBLIC_API_ENDPOINT + '/auth/login', {
+    access_token,
+    token_type,
+    scope,
+  });
+
+  const { data: userinfo } = await axios.get('https://api.github.com/user', {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  return { data, userinfo };
 }
