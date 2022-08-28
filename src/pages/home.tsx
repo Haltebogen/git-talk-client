c9f65d5797ce4cc440ce1a9e6751e7af00fe8c23
Kimkyungmin123 <rud3302@naver.com>
2022-08-29 04:52:43 +0900

    chore: search new member관리

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,23 @@
 import AddSomethingModal from '@/molecules/addSomething/AddSomethingModal';
+import SearchResult from '@/molecules/search/SearchResult';
 import FollowerList from '@/organisms/followerList/FollowerList';
 import FollowerProfile from '@/organisms/followerProfile/FollowerProfile';
 import NavBarLayout from '@/organisms/navBar/NavBarLayout';
 import useInput from 'hooks/useInput';
 import useModal from 'hooks/useModal';
+import useSearch from 'hooks/useSearch';
 import { NextPage } from 'next';
+import { useState } from 'react';
 import { Container, Area } from 'styles/home';
+import subInstance from 'utils/api/sub';
 
 const Home: NextPage = () => {
   const { isShown, openModal, closeModal } = useModal();
   const [value, onChangeValue, setValue] = useInput('');
   const [newMember, onChangeNewMember, setNewMember] = useInput('');
+  const { searchResult } = useSearch(newMember, false);
+  const [memberId, setMemberId] = useState(0);
+  console.log(searchResult);
   return (
     <Container>
       <NavBarLayout title="Git-Talk _ 홈">
@@ -31,10 +38,14 @@ const Home: NextPage = () => {
       </NavBarLayout>
       <AddSomethingModal
         show={isShown}
-        onClick={() => setNewMember('')}
-        onCloseModal={closeModal}
+        onCloseModal={() => {
+          closeModal();
+          setNewMember('');
+        }}
         onSubmit={(event) => {
           event.preventDefault();
+          subInstance.createFollow(memberId);
+          setNewMember('');
         }}
         placeholder="이메일 혹은 아이디를 입력하세요"
         onChange={(event) => {
@@ -43,7 +54,22 @@ const Home: NextPage = () => {
           setNewMember(value);
         }}
         value={newMember}
-      />
+        onCancelClick={() => setNewMember('')}
+      >
+        <SearchResult>
+          {searchResult?.map((data: any) => (
+            <li
+              key={data.providerId}
+              onClick={() => {
+                setNewMember(data.nickName);
+                setMemberId(data.providerId);
+              }}
+            >
+              <span>{data.nickName}</span>
+            </li>
+          ))}
+        </SearchResult>
+      </AddSomethingModal>
     </Container>
   );
 };
