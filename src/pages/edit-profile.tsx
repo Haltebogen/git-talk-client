07261bda5f53d7e4c95047b63c0b901eaa6b3c9c
Kimkyungmin123 <rud3302@naver.com>
2022-08-29 23:27:49 +0900

    feat: edit profile 기능 추가

--- src/pages/edit-profile.tsx
+++ src/pages/edit-profile.tsx
@@ -1,13 +1,50 @@
 import EditMyProfile from '@/organisms/editMyProfile/EditMyProfile';
 import NavBarLayout from '@/organisms/navBar/NavBarLayout';
-
+import useInput from 'hooks/useInput';
 import { NextPage } from 'next';
+import { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { State } from 'store/configureStore';
+import { IUserState, setUser } from 'store/features/userSlice';
+import subInstance from 'utils/api/sub';
 
 const Editprofile: NextPage = () => {
+  const [statusValue, onStatusChange, setStatusValue] = useInput('');
+  const [bioValue, onBioChange, setBioValue] = useInput('');
+  const { name, nickName, profileImageUrl, statusMessage, bio } = useSelector<State, IUserState>((state) => state.user);
+  const dispatch = useDispatch();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const onImageChange = () => {
+    console.log('change');
+  };
+  const onEditFileClick = () => {
+    console.log('chilck');
+  };
+
   return (
     <div>
       <NavBarLayout title="Git-Talk _ 프로필 수정">
-        <EditMyProfile />
+        <EditMyProfile
+          onEditFileClick={onEditFileClick}
+          onImageChange={onImageChange}
+          fileInputref={fileInputRef}
+          bioPlaceholder={bio}
+          statusPlaceholder={statusMessage}
+          name={name}
+          nickName={nickName}
+          profileImageUrl={profileImageUrl}
+          onSubmit={(event) => {
+            event.preventDefault();
+            subInstance.updateProfile(statusValue, bioValue);
+            setStatusValue('');
+            setBioValue('');
+            dispatch(setUser({ name, nickName, profileImageUrl, statusMessage: statusValue, bio: bioValue }));
+          }}
+          statusValue={statusValue}
+          onStatusChange={onStatusChange}
+          bioValue={bioValue}
+          onBioChange={onBioChange}
+        />
       </NavBarLayout>
     </div>
   );
