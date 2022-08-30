33f190663927d02767b0165516bd1d5998d4266f
Kimkyungmin123 <rud3302@naver.com>
2022-08-31 00:08:30 +0900

    chore: memberSlice 추가

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -2,13 +2,16 @@ import { createWrapper } from 'next-redux-wrapper';
 import { combineReducers, configureStore, getDefaultMiddleware, Store } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import userSlice, { IUserState } from './features/userSlice';
+import memberSlice, { IMemberState } from './features/memberSlice';
 
 export interface State {
   user: IUserState;
+  member: IMemberState;
 }
 
 export const rootReducer = combineReducers({
   user: userSlice,
+  member: memberSlice,
 });
 
 const createStore = () => {
---
+++ src/store/features/memberSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Imember } from 'type';
+
+export interface IMemberState {
+  bio?: string | null;
+  company?: string | null;
+  email?: string | null;
+  followersNum?: number | null;
+  followingsNum?: number | null;
+  name?: string | null;
+  nickName?: string | null;
+  profileImageUrl?: string | null;
+  statusMessage?: string | null;
+}
+
+const initialState: IMemberState = {
+  bio: null,
+  company: null,
+  email: null,
+  followersNum: null,
+  followingsNum: null,
+  name: null,
+  nickName: null,
+  profileImageUrl: null,
+  statusMessage: null,
+};
+
+export const memberSlice = createSlice({
+  name: 'member',
+  initialState,
+  reducers: {
+    setMember: (_, action: PayloadAction<IMemberState>) => action.payload,
+    extraReducers: (builder) => builder,
+  },
+});
+
+export const { setMember } = memberSlice.actions;
+
+export default memberSlice.reducer;
