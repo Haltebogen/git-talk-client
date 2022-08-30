235f3616cdef5ecc4073f4011f8836c654d12a60
Kimkyungmin123 <rud3302@naver.com>
2022-08-31 00:07:16 +0900

    chore: notification style변경

--- src/styles/Notification.ts
+++ src/styles/Notification.ts
@@ -1,4 +1,5 @@
 import { BoxLayout } from '@/boxes/Box';
+import { ButtonLayout } from '@/buttons/Button';
 import Image from 'next/image';
 import styled, { css } from 'styled-components';
 
@@ -15,6 +16,7 @@ export const NotificationBox = styled(BoxLayout)`
   ${({ theme }) => {
     const { colors } = theme;
     return css`
+      position: relative;
       display: flex;
       flex-direction: column;
       border-radius: 0.375rem;
@@ -22,25 +24,25 @@ export const NotificationBox = styled(BoxLayout)`
       background-color: ${colors.secondary};
       display: flex;
       width: 100%;
-      padding: 0.9375rem;
+      padding: 2.5rem;
       gap: 0.625rem;
+
+      &:hover {
+        cursor: default;
+      }
     `;
   }}
 `;
 
 export const NotiTitle = styled.div`
   ${({ theme }) => {
-    const { colors } = theme;
+    const { colors, fontSize } = theme;
     return css`
-      display: flex;
-      align-items: center;
-      min-width: 28.125rem;
+      position: absolute;
+      top: 0.625rem;
+      left: 1rem;
       color: ${colors.gray};
-
-      h2 {
-        padding-right: 2.5rem;
-        margin: 0;
-      }
+      font-size: ${fontSize.xxs};
     `;
   }}
 `;
@@ -49,9 +51,9 @@ export const UserInfo = styled.div`
   ${({ theme }) => {
     const { colors, fontSize } = theme;
     return css`
-      display: flex;
-      align-items: center;
-
+      position: absolute;
+      top: 3rem;
+      left: 1.5rem;
       span {
         padding: 0.625rem 0 0 0.625rem;
         font-size: ${fontSize.xxs};
@@ -74,15 +76,23 @@ export const ProfileImage = styled(Image)`
 `;
 
 export const NotiDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 31.25rem;
-  padding-top: 0.625rem;
+  ${({ theme }) => {
+    const { colors, fontSize } = theme;
+    return css`
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      max-width: 31.25rem;
+      padding-top: 0.625rem;
+      color: ${colors.purple};
+      font-size: ${fontSize.md};
+      font-weight: 700;
+    `;
+  }}
 `;
 
-export const Date = styled.div`
-  width: 100%;
-  max-width: 18.75rem;
-  padding-top: 0.625rem;
+export const AllowButton = styled(ButtonLayout)`
+  position: absolute;
+  right: 1.875rem;
+  max-width: 4.375rem;
 `;
