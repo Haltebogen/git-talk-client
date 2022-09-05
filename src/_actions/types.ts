a176ad5dc6d1c387ec3ecd432d7389ee86864aa6
oereo <dlstpgns0406@gmail.com>
2022-09-05 22:56:14 +0900

    feat: redux action types 정의

---
+++ src/_actions/types.ts
@@ -0,0 +1,4 @@
+export const NEW_CHAT_ROOM = "new_chat_room";
+export const LEAVE_CHAT_ROOM = "leave_chat_room";
+export const SEND_MESSAGE = "send_message";
+export const RECEIVE_MESSAGE = "receive_message";
