962fd2b9e5131da6f9093fafd66a8f235154f713
oereo <dlstpgns0406@gmail.com>
2022-09-06 17:37:38 +0900

    refactor: function format 수정으로 인한 method 제거

--- src/_actions/types.ts
+++ src/_actions/types.ts
@@ -1,20 +1,4 @@
-const NEW_CHAT_ROOM = "new_chat_room" as const;
-const LEAVE_CHAT_ROOM = "leave_chat_room" as const;
-const SEND_MESSAGE = "send_message" as const;
-const RECEIVE_MESSAGE = "receive_message" as const;
-
-export const newChatRoom = () => ({
-  type: NEW_CHAT_ROOM
-});
-
-export const leaveChatRoom = () => ({
-  type: LEAVE_CHAT_ROOM
-});
-
-export const sendMessage = () => ({
-  type: SEND_MESSAGE
-});
-
-export const receiveMessage = () => ({
-  type: RECEIVE_MESSAGE
-});
+export const NEW_CHAT_ROOM = "new_chat_room" as const;
+export const LEAVE_CHAT_ROOM = "leave_chat_room" as const;
+export const SEND_MESSAGE = "send_message" as const;
+export const RECEIVE_MESSAGE = "receive_message" as const;
