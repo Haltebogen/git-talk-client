bf76d9695e6bcc0969a0271df769e329ce04d28f
oereo <dlstpgns0406@gmail.com>
2022-09-06 17:40:08 +0900

    feat: sendMessage, receiveMessage, leaveChatRoom function 재정의

--- src/_actions/ChatAction.ts
+++ src/_actions/ChatAction.ts
@@ -1,7 +1,22 @@
 import { NEW_CHAT_ROOM, LEAVE_CHAT_ROOM, RECEIVE_MESSAGE, SEND_MESSAGE } from "./types";
-import { TargetChatRoom } from "./ChatActionTypes";
+import { TargetChatRoom, TargetMessage } from "./ChatActionTypes";
 
 export const createChatRoom = (targetChatRoom: TargetChatRoom) => ({
   type: NEW_CHAT_ROOM,
   payload: targetChatRoom,
+});
+
+export const leaveChatRoom = (targetChatRoom: TargetChatRoom) => ({
+  type: LEAVE_CHAT_ROOM,
+  payload: targetChatRoom,
+});
+
+export const sendMessage = (targetMessage: TargetMessage) => ({
+  type: SEND_MESSAGE,
+  payload: targetMessage,
+});
+
+export const receiveMessage = (targetMessage: TargetMessage) => ({
+  type: RECEIVE_MESSAGE,
+  payload: targetMessage,
 });
