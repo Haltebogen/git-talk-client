92e5606c4571e85f69444bdc83a72dd5681ae607
oereo <dlstpgns0406@gmail.com>
2022-09-06 14:15:53 +0900

    refactor: constants export 형식에서 function export 형식으로 수정

--- src/_actions/types.ts
+++ src/_actions/types.ts
@@ -1,4 +1,20 @@
-export const NEW_CHAT_ROOM = "new_chat_room" as const;
-export const LEAVE_CHAT_ROOM = "leave_chat_room" as const;
-export const SEND_MESSAGE = "send_message" as const;
-export const RECEIVE_MESSAGE = "receive_message" as const;
+const NEW_CHAT_ROOM = "new_chat_room" as const;
+const LEAVE_CHAT_ROOM = "leave_chat_room" as const;
+const SEND_MESSAGE = "send_message" as const;
+const RECEIVE_MESSAGE = "receive_message" as const;
+
+export const new_chat_room = () => ({
+  type: NEW_CHAT_ROOM
+});
+
+export const leave_chat_room = () => ({
+  type: LEAVE_CHAT_ROOM
+});
+
+export const send_message = () => ({
+  type: SEND_MESSAGE
+});
+
+export const receive_message = () => ({
+  type: RECEIVE_MESSAGE
+});
