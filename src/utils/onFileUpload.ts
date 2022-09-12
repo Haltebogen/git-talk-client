18675a32b4a3897cef193fea54174364f9d3e23d
Kimkyungmin123 <rud3302@naver.com>
2022-09-12 23:43:27 +0900

    chore: util함수 - onFileUpload 수정

--- src/utils/onFileUpload.ts
+++ src/utils/onFileUpload.ts
@@ -1,12 +1,15 @@
 import AWS from 'aws-sdk';
+import subInstance from './api/sub';
+
+const AWS_S3_PREFIX = 'https://git-talk.s3.ap-northeast-2.amanzonaws.com/';
 
 const onFileUpload = (e: any) => {
   const REGION = 'ap-northeast-2';
   const S3_BUCKET = 'git-talk';
 
   AWS.config.update({
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
   });
 
   const myBucket = new AWS.S3({
@@ -20,14 +23,15 @@ const onFileUpload = (e: any) => {
     ACL: 'public-read',
     Body: file,
     Bucket: S3_BUCKET,
-    Key: file.name,
+    Key: `${new Date().getTime()}-${file.name}`,
   };
 
   myBucket
     .putObject(params)
-    .on('httpUploadProgress', (evt) => {
-      alert('SUCCESS');
+    .on('httpUploadProgress', () => {
+      subInstance.updateProfileImage(AWS_S3_PREFIX + params.Key);
     })
+
     .send((err) => {
       if (err) console.log(err);
     });
