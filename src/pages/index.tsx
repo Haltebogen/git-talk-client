254558069582dd23f78fa27cce21fb532b627277
Kimkyungmin123 <rud3302@naver.com>
2022-10-04 04:52:18 +0900

    chore: 미사용 import 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import HomeLogo from '@/logos/HomeLogo';
-import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
+import type { NextPage } from 'next';
 import { Container, LoginButton, TextContainer } from '@/styles/index';
 import Head from 'next/head';
-import { initUser } from 'store/features/userSlice';
-import wrapper from 'store/configureStore';
 
 const Login: NextPage = () => {
   const loginUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_ID}&redirect_uri=${process.env.NEXT_PUBLIC_CLIENT_URL}/api/auth/github/callback`;
